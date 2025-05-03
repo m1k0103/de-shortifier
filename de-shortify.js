43e26db48ce9f2fdaa564311bdfb535f9227c3d4
mikok <84585119+miko1034@users.noreply.github.com>
2025-05-03 16:07:49 +0100

    removes the shorts element and also if you find yourself on a short, goes back, refreshes page, and removes the short

--- de-shortify.js
+++ de-shortify.js
@@ -1,19 +1,28 @@
 //console.clear()
 
-setInterval(function(){
-    if (window.location.href.includes("shorts")){
-        //history.back()
-        document.location = "https://www.youtube.com"
+function remove_shorts(){
+    // removes shorts from homepage
+    let to_hide = document.querySelectorAll("ytm-shorts-lockup-view-model-v2, ytm-shorts-lockup-view-model")
+    console.log(to_hide)
+    //debugger
+    for (let i=0; i < to_hide.length; i++){
+        to_hide[i].remove()
+        console.log("removed shorts")
+    }
+}
 
-        // removes shorts from homepage
-        //let to_hide = document.querySelectorAll("ytd-rich-section-renderer, .ytd-rich-section-renderer")
-        //console.log(to_hide)
-        //for (let i=0; i < to_hide.length; i++){
-        //    to_hide[i]
-        //    console.log("removed shorts")
-        //}
+
+// interval with a function containing the remove_shorts function and little additional redirect code.
+setInterval(function(){
+    remove_shorts()
+
+    // takes you back to before you clicked the short
+    if (window.location.href.includes("shorts")){
+        history.back()
+        document.location = document.location // refreshes page
+        //document.location = "https://www.youtube.com"
 
     }
-}, 1000)
+}, 10000)
 
 
